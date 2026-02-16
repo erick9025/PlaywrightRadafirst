54e618951efe00afa1614886459f370fc608b5d7
Erick Jiménez <erick9025@hotmail.com>
2026-02-16 11:46:17 -0600

    Working

--- tests/test1.test.js
+++ tests/test1.test.js
@@ -4,48 +4,38 @@ let browser;
 let context;
 let page;
 
-// Basically we have 4 test annotation
-// ... beforeAll()
-// ... beforeEach()
-// ... afterEach()
-// ... afterAll()
-
 ////////////////////////////////////////////////////////// BEFORE/AFTER SETUP //////////////////////////////////////////////////////////
-
 test.beforeAll(async () => {
     // Launch browser
     browser = await chromium.launch({ headless: false });  
 });
 
-test.beforeEach(async () => {    
+test.beforeEach(async () => {
     // Create a BrowserContext (isolated session)
     context = await browser.newContext();
 
     // Create a Page inside the context
     page = await context.newPage();    
-    await page.waitForTimeout(3_000); // 5 seconds to allow me move windows from not shown screen to shared screen
+    //await page.waitForTimeout(3_000); // 5 seconds to allow me move windows from not shown screen to shared screen
 });
 
-/*test.afterEach(async () => {    
+test.afterEach(async () => {    
     await context.close();
 });
 
-test.beforeAll(async () => {
+test.afterAll(async () => {
     await browser.close();
-});*/
+});
 
 /////////////////////////////////////////////////////////// TESTS START HERE ///////////////////////////////////////////////////////////
-
-test("My very first test", async () => {
-    // Navigate to Google
+test("My very first test with Facebook", async () => {
+    // Navigate to Facebook
     await page.goto("https://www.facebook.com");
 
-    // Enter something
+    // Go to form, then enter person name information
     await page.locator("[data-testid='open-registration-form-button']").click();
     await page.locator("[name='firstname']").fill("Erick");
+    await page.locator("[name='lastname']").fill("Jiménez");
 
-    console.log('Test completed');  
-    
-    await context.close();
-    await browser.close();
+    console.log('Test completed by ERICK JIMENEZ');  
 });
