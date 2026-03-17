921ac29b7094fec9b517e37bfef84d6804891a37
Erick Jiménez <erick9025@hotmail.com>
2026-03-17 10:59:14 -0600

    Still trying

--- tests/swagTests/testSwagLoadedContext.test.ts
+++ tests/swagTests/testSwagLoadedContext.test.ts
@@ -4,5 +4,5 @@ test('test with persistent profile', async ({ persistentPage }) => {
   if (!persistentPage) test.skip();
 
   await persistentPage!.goto('https://facebook.com');
-  await persistentPage!.waitForTimeout(5_000); // Wait for 5 seconds to observe the page
+  await persistentPage!.waitForTimeout(15_000); // Wait for 5 seconds to observe the page
 });
